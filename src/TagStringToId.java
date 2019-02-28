7a143dfd8b5998c065c5cc4980d03134585aa878
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2019-02-28 20:28:16 +0200

    wip

---
+++ src/TagStringToId.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TagStringToId {
+
+    Map<String, Integer> tagToStringMap;
+    Integer id;
+
+
+    public TagStringToId() {
+        id = 0;
+        tagToStringMap = new HashMap<>();
+    }
+
+    public Integer getIdOfTag(String tag) {
+        return null;
+    }
+
+}
