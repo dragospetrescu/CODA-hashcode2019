358394da578da3f153eefebe4195d15fad54cff2
Claudiu Marinescu <claudiu.marinescu95@stud.acs.upb.ro>
2019-02-28 20:55:19 +0200

    reading functionality

--- src/util/Type.java
+++ src/util/Type.java
@@ -2,7 +2,16 @@ package util;
 
 public enum Type {
 
-    H,
-    V
+    H("H"),
+    V("V");
 
+    private String description;
+
+    Type(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
