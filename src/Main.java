81e8a2403297beb1dc243abfd79720233fe0491e
Claudiu Marinescu <claudiu.marinescu95@stud.acs.upb.ro>
2019-02-28 13:03:27 +0200

    added util classes from last year

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,95 @@
+import util.MyScanner;
+import util.Util;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+
+        int outIdx = 0;
+
+        for (String file : Util.INPUT_FILES) {
+            System.out.println("Computing " + file);
+
+            read(Util.INPUT_FOLDER + file);
+            computeAlgo();
+            writeResults(Util.OUTPUT_FOLDER + Util.OUTPUT_FILES[outIdx++]);
+        }
+
+        System.out.println("Results are ready!");
+
+
+    }
+
+
+    /**
+     * Compute some algorithm to solve the problem.
+     *
+     *
+     */
+    private static void computeAlgo() {
+
+    }
+
+
+
+    /**
+     * Read input data from file.
+     *
+     *
+     * @param filename file to read from
+     */
+    private static void read(String filename) {
+        MyScanner scanner = new MyScanner(new File(filename));
+
+        Util.WIDTH = scanner.nextInt();
+        Util.HEIGHT = scanner.nextInt();
+    }
+
+
+    /**
+     * Write results to file.
+     *
+     *
+     * @param outFile file to write to
+     */
+    private static void writeResults(String outFile) {
+
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+
+        try {
+
+            fw = new FileWriter(new File(outFile));
+            bw = new BufferedWriter(fw);
+
+            StringBuilder content = new StringBuilder();
+
+            // compute content
+
+            bw.write(content.toString());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null)
+                    bw.close();
+
+                if (fw != null)
+                    fw.close();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
 }
---
+++ src/util/Util.java
@@ -0,0 +1,30 @@
+package util;
+
+public class Util {
+
+    public static int WIDTH;
+    public static int HEIGHT;
+
+
+    public static final String INPUT_FOLDER = "input\\";
+
+    public static final String OUTPUT_FOLDER = "output\\";
+
+    public static final String[] OUTPUT_FILES = {
+            "a.out",
+            "b.out",
+            "c.out",
+            "d.out",
+            "e.out"
+    };
+
+    public static final String[] INPUT_FILES = {
+            "a_example.in",
+            "b_should_be_easy.in",
+            "c_no_hurry.in",
+            "d_metropolis.in",
+            "e_high_bonus.in"
+    };
+
+
+}
