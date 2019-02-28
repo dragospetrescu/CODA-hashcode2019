6978f8118dff86b382ca569964017ee39ab3eb9e
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2019-02-28 20:53:45 +0200

    made scorer needs testing

--- src/util/Scorer.java
+++ src/util/Scorer.java
@@ -2,30 +2,36 @@ package util;
 
 import pics.Pic;
 
-import java.util.List;
-
 public class Scorer {
 
     public static int getScore(Pic pic1, Pic pic2) {
         int common = 0;
-        int uncommon1 = 0;
-        int uncommon2 = 0;
+        int uniq1 = 0;
+        int uniq2 = 0;
 
         int[] tags1 = pic1.getTags();
         int[] tags2 = pic2.getTags();
 
-        for (Integer tag1: tags1) {
-            for (Integer tag2: tags2) {
-                if(tag2 > tag1) {
-                    break;
-                }
+        // TODO: MAKE THE SMALLER LIST SECOND
+        for (int tag1: tags1) {
+            for (int tag2: tags2) {
                 if(tag2 == tag1) {
                     common++;
+                    break;
+                }
+                if(tag2 > tag1) {
+                    uniq1++;
                     break;
                 }
             }
 
         }
+
+        int numTotalEls = tags1.length + tags2.length;
+        uniq2 = numTotalEls - common * 2 - uniq1;
+
+        int min_temp = Math.min(common, uniq1);
+        return Math.min(min_temp, uniq2);
     }
 
 }
